15ec4eb26b01b6f339efd24947c74c4f482a8fec
Vasileios Papastergios <papastva@csd.auth.gr>
2022-11-09 00:03:31 +0200

    Uploaded new sound clips (CC) and added them in the Constants class.

--- src/Constants.java
+++ src/Constants.java
@@ -3,5 +3,9 @@ package src;
 public class Constants {
     public static final String FONT_FILES_PATH = "src/resources/Fonts/";
 
-    public static final String GAME_OVER_SOUND = "src/resources/SoundClips/wav2.wav";
+    public static final String GAME_START_SOUND_CLIP = "src/resources/SoundClips/game_start.wav";
+
+    public static final String GAME_OVER_SOUND_CLIP = "src/resources/SoundClips/game_over.wav";
+
+    public static final String BUTTON_CLICK_SOUND_CLIP = "src/resources/SoundClips/button_clicked.wav";
 }
