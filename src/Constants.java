53f2b642f37c021fd501b320359383718b6504af
Vasileios Papastergios <papastva@csd.auth.gr>
2022-11-08 22:05:02 +0200

    Created a simple class to organize constant values, file paths and files used in the game.

---
+++ src/Constants.java
@@ -0,0 +1,7 @@
+package src;
+
+public class Constants {
+    public static final String FONT_FILES_PATH = "src/resources/Fonts/";
+
+    public static final String GAME_OVER_SOUND = "src/resources/SoundClips/wav2.wav";
+}
