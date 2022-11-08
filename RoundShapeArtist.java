f56567c57a0848d03285cbab30b7fd770d12f7a2
Vasileios Papastergios <papastva@csd.auth.gr>
2022-11-08 10:15:18 +0200

    Developed code for a rounded shapes and buttons.

---
+++ RoundShapeArtist.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+/**
+ * An auxiliary class that provides a static method, in order to draw round-shaped components.
+ * The class is utilized by the customized round buttons and round panels of the game, in order to
+ * feature uniformity, in terms of design.
+ *
+ * @author Vasileios Papastergios
+ */
+public class RoundShapeArtist {
+
+    public static void drawRoundedShapeAndStroke(JComponent component, Dimension arcs, Graphics g) {
+        int strokeSize = 1;
+        Color shadowColor = Color.BLACK;
+        boolean shady = true;
+        boolean highQuality = true;
+        int shadowGap = 5;
+        int shadowOffset = 4;
+        int shadowAlpha = 150;
+
+        int width = component.getWidth();
+        int height = component.getHeight();
+
+        Color shadowColorA = new Color(shadowColor.getRed(), shadowColor.getGreen(), shadowColor.getBlue(), shadowAlpha);
+        Graphics2D graphics = (Graphics2D) g;
+
+        if (highQuality) {
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        }
+
+        if (shady) {
+            graphics.setColor(shadowColorA);
+            graphics.fillRoundRect(shadowOffset, shadowOffset, width - strokeSize - shadowOffset, height - strokeSize - shadowOffset, arcs.width, arcs.height);
+        } else {
+            shadowGap = 1;
+        }
+
+        graphics.setColor(component.getBackground());
+        graphics.fillRoundRect(0, 0, width - shadowGap, height - shadowGap, arcs.width, arcs.height);
+        graphics.setColor(component.getForeground());
+        graphics.setStroke(new BasicStroke(strokeSize));
+        graphics.drawRoundRect(0, 0, width - shadowGap,
+                height - shadowGap, arcs.width, arcs.height);
+
+        graphics.setStroke(new BasicStroke());
+    }
+}
---
+++ RoundedJButton.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * A simple class that represents a round-shaped button in our game. Objects of this class are utilized in
+ * the answer buttons displayed to the player(s). The class is developed as a sub-class of the <code>JButton</code>
+ * class.
+ *
+ * @author Vasileios Papapstergios
+ */
+public class RoundedJButton extends JButton {
+    private Dimension arcs;
+
+    /**
+     * Constructs a rounded button with given arc dimensions.
+     *
+     * @param arcs the dimensions for the button arcs.
+     */
+    public RoundedJButton(Dimension arcs) {
+        this.arcs = arcs;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D graphics = (Graphics2D) g;
+        RoundShapeArtist.drawRoundedShapeAndStroke(this, arcs, g);
+
+        FontMetrics fm = graphics.getFontMetrics();
+        int x = (getWidth() - fm.stringWidth(getText())) / 2;
+        int y = ((getHeight() - fm.getHeight()) / 2) + fm.getAscent();
+        graphics.drawString(getText(), x, y);
+
+        setBorderPainted(false);
+    }
+}
