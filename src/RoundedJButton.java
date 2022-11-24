f62b4279d952be3ea6a766be8e2cbaa2643f72cc
Vasileios Papastergios <papastva@csd.auth.gr>
2022-11-24 20:46:31 +0200

    Added code for enchanced user GUI regarding the main menu buttons

--- src/RoundedJButton.java
+++ src/RoundedJButton.java
@@ -2,6 +2,8 @@ package src;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 /**
  * A simple class that represents a round-shaped button in our game. Objects of this class are utilized in
@@ -21,6 +23,7 @@ public class RoundedJButton extends JButton {
     public RoundedJButton(Dimension arcs) {
         this.arcs = arcs;
         setOpaque(false);
+        this.addMouseListener(new CustomMouseAdapter());
     }
 
     @Override
@@ -34,5 +37,44 @@ public class RoundedJButton extends JButton {
         graphics.drawString(getText(), x, y);
 
         setBorderPainted(false);
+    }
+
+    protected class CustomMouseAdapter extends MouseAdapter {
+        private static final int RELOCATION_X = 0;
+        private static final int RELOCATION_Y = 6;
+        private static final int RELOCATION_WIDTH = 6;
+        private static final int RELOCATION_HEIGHT = 6;
+
+        @Override
+        public void mouseEntered(MouseEvent e) {
+            JButton buttonSource = (JButton) e.getSource();
+            inverseColors(buttonSource);
+        }
+
+        @Override
+        public void mouseExited(MouseEvent e) {
+            JButton buttonSource = (JButton) e.getSource();
+            inverseColors(buttonSource);
+        }
+
+        @Override
+        public void mouseClicked(MouseEvent e) {
+            JButton buttonSource = (JButton) e.getSource();
+            buttonSource.setBounds(getX() - RELOCATION_X, getY() - RELOCATION_Y, getWidth() + RELOCATION_WIDTH, getHeight() + RELOCATION_HEIGHT);
+            buttonSource.repaint();
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            JButton buttonSource = (JButton) e.getSource();
+            buttonSource.setBounds(getX() + RELOCATION_X, getY() + RELOCATION_Y, getWidth() - RELOCATION_WIDTH, getHeight() - RELOCATION_HEIGHT);
+            buttonSource.repaint();
+        }
+
+        private void inverseColors(JButton buttonSource) {
+            Color background = buttonSource.getBackground();
+            buttonSource.setBackground(buttonSource.getForeground());
+            buttonSource.setForeground(background);
+        }
     }
 }
